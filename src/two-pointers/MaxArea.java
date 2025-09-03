33dcbf7826b1fbe9ff45bdbd6c187b74c2a1b506
mattbixby123 <matthew.bixby1@gmail.com>
2025-09-03 15:33:36 -0400

    feat: add three Blind 75 problems with optimal solutions
    - LC 121: Best Time to Buy and Sell Stock (sliding window)
    - LC 11: Container With Most Water (two pointers)
    - LC 15: 3Sum (two pointers with duplicate handling)
    - Update .gitignore for Java project structure

---
+++ src/two-pointers/MaxArea.java
@@ -0,0 +1,20 @@
+// leetcode # 11 - container with most water
+public class MaxArea {
+    public int maxArea(int[] heights) {
+        int l = 0, r = heights.length - 1;
+        int res = 0;
+
+        while (l < r) {
+            int current = Math.min(heights[l], heights[r]) * (r - l);
+            res = Math.max(res, current);
+
+            if (heights[l] < heights[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return res;
+    }
+}
