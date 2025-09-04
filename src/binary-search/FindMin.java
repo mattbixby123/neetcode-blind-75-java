86d5f22fcf5453f0e91e61d83d8d20b9496b4ad9
mattbixby123 <matthew.bixby1@gmail.com>
2025-09-04 18:57:26 -0400

    Add/Modify solutions for 9 Blind 75 problems
    - Binary search: FindMin, Search in rotated array
    - Sliding window: Character replacement, longest substring, max profit, min window
    - Stacks: Valid parentheses
    - Two pointers: Container with most water, 3Sum

---
+++ src/binary-search/FindMin.java
@@ -0,0 +1,18 @@
+// leetcode # 153 - find minimum in rotated sorted array
+// binary search - O(log n) time O(1) space
+public class FindMin {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return nums[l];
+    }
+}
